ab6b139df250503bd4b70c26056b3911695decc8
Matias Mamani <josmamani848.inf@industrialmurillo.edu.bo>
2026-06-27 11:26:13 -0400

    Revert "Agrega función de saludo"
    This reverts commit affdf1e0a9446fad435ec3129009bbc051584df7.

--- saludo.py
+++
@@ -1,6 +0,0 @@
-def saludar(nombre_completo):
-    if not nombre_completo or nombre_completo.strip() == "":
-        return "Error: el nombre no puede estar vacío."
-    return f"Hola, {nombre_completo}!"
-
-print(saludar("GitHub"))
